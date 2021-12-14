a31fa9a7d2a0ba04fa8ba7a72e91c741237d82de
Phil Goodwin <phil.goodwin@gmail.com>
2021-12-13 20:28:56 -0800

    Day 4 -- complete

--- src/main/kotlin/day4.kt
+++ src/main/kotlin/day4.kt
@@ -1,28 +1,96 @@
 import java.io.File
 
+class Board(val positionValues: List<List<Int>>) {
+    fun isWinner(pickedNumbers: List<Int>): Boolean {
+        (0..4).forEach { if (pickedNumbers.containsAll(positionValues[it])) return true }
+        (0..4).forEach { v -> if (pickedNumbers.containsAll(positionValues.map { it[v] })) return true }
+        return false
+    }
+
+    fun score(pickedNumbers: List<Int>): Int {
+        return positionValues.flatten().filter { !pickedNumbers.contains(it) }.sum()
+    }
+
+    override fun toString(): String {
+        val rows = StringBuilder()
+        rows.append(positionValues[0].fold("") { acc, num -> "$acc $num" })
+        rows.append("\n")
+        rows.append(positionValues[1].fold("") { acc, num -> "$acc $num" })
+        rows.append("\n")
+        rows.append(positionValues[2].fold("") { acc, num -> "$acc $num" })
+        rows.append("\n")
+        rows.append(positionValues[3].fold("") { acc, num -> "$acc $num" })
+        rows.append("\n")
+        rows.append(positionValues[4].fold("") { acc, num -> "$acc $num" })
+        rows.append("\n")
+        return rows.toString()
+    }
+
+}
+
 fun main() {
     val bingoData = File("bingo.txt").readLines().iterator()
     val bingoBalls = bingoData.next().split(",").map(String::toInt)
 
     val boardRegEx = "\\w*(\\d+)".toRegex()
-    val boards = mutableListOf<List<List<Int>>>()
+    val boards = mutableListOf<Board>()
     while (bingoData.hasNext()) {
         bingoData.next() // skip blank line
-        boards.add(listOf(
-            boardRegEx.findAll(bingoData.next()).toList().map{ it.value.toInt()},
-            boardRegEx.findAll(bingoData.next()).toList().map{ it.value.toInt()},
-            boardRegEx.findAll(bingoData.next()).toList().map{ it.value.toInt()},
-            boardRegEx.findAll(bingoData.next()).toList().map{ it.value.toInt()},
-            boardRegEx.findAll(bingoData.next()).toList().map{ it.value.toInt()},
-        ))
+        boards.add(
+            Board(
+                listOf(
+                    boardRegEx.findAll(bingoData.next()).toList().map { it.value.toInt() },
+                    boardRegEx.findAll(bingoData.next()).toList().map { it.value.toInt() },
+                    boardRegEx.findAll(bingoData.next()).toList().map { it.value.toInt() },
+                    boardRegEx.findAll(bingoData.next()).toList().map { it.value.toInt() },
+                    boardRegEx.findAll(bingoData.next()).toList().map { it.value.toInt() },
+                )
+            )
+        )
     }
-    println(bingoBalls)
-    boards.forEach { board ->
-        println()
-        println(board[0])
-        println(board[1])
-        println(board[2])
-        println(board[3])
-        println(board[4])
+
+    printFirstWinnerReport(bingoBalls, boards)
+    printLastWinnerReport(bingoBalls, boards)
+}
+
+fun printLastWinnerReport(bingoBalls: List<Int>, boards: MutableList<Board>) {
+    val remainingBoards = mutableListOf<Board>()
+    remainingBoards.addAll(boards)
+    val pickedNumbers = mutableListOf<Int>()
+    bingoBalls.forEach { picked ->
+        pickedNumbers.add(picked)
+        boards.forEach { board ->
+            if (board.isWinner(pickedNumbers)) {
+                remainingBoards.remove(board)
+                if (remainingBoards.size == 0) {
+                    println(board)
+                    println(pickedNumbers)
+                    println(board.score(pickedNumbers))
+                    println(pickedNumbers.last())
+                    println(board.score(pickedNumbers) * pickedNumbers.last())
+                    return
+                }
+            }
+        }
+    }
+}
+
+private fun printFirstWinnerReport(
+    bingoBalls: List<Int>,
+    boards: MutableList<Board>
+) {
+    val pickedNumbers = mutableListOf<Int>()
+    bingoBalls.forEach { picked ->
+        pickedNumbers.add(picked)
+        boards.forEach { board ->
+            if (board.isWinner(pickedNumbers)) {
+                println(board)
+                println(pickedNumbers)
+                println(board.score(pickedNumbers))
+                println(pickedNumbers.last())
+                println(board.score(pickedNumbers) * pickedNumbers.last())
+                return
+            }
+        }
     }
 }
