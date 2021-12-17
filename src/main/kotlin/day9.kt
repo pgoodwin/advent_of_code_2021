cdbf12d5fc15aebfbdc17752e0a8952c9bf6bcf8
Phil Goodwin <phil.goodwin@gmail.com>
2021-12-16 18:17:09 -0800

    Day 9 -- complete

--- src/main/kotlin/day9.kt
+++ src/main/kotlin/day9.kt
@@ -10,12 +10,38 @@ fun main() {
         }
     }
 
-    heightAtPosition.keys.filter { isLowPoint(it, heightAtPosition) }.sumOf{ (heightAtPosition[it]!! + 1)} }
+    val lowPoints = heightAtPosition.keys.filter { isLowPoint(it, heightAtPosition) }
+    lowPoints.sumOf { (heightAtPosition[it]!! + 1) }.also(::println)
+
+    lowPoints.map { buildBasin(it, heightAtPosition) }
+        .sortedByDescending { it.size }
+        .take(3)
+        .map {it.size}
+        .reduce { acc, value -> acc * value }
+        .also(::println)
 }
 
-private fun min(vararg ints: Int?): Int {
+fun buildBasin(
+    lowPoint: Pair<Int, Int>,
+    heightAtPosition: MutableMap<Pair<Int, Int>, Int>,
+    existingBasin: List<Pair<Int, Int>> = mutableListOf(lowPoint)
+): List<Pair<Int, Int>> {
+    val newBasinPoints =
+        adjacentPoints(lowPoint).filter { (heightAtPosition[it] ?: 9) != 9 && !existingBasin.contains(it) }
+    val biggerBasin = existingBasin + newBasinPoints
+    return newBasinPoints.fold(biggerBasin) { basin, point -> buildBasin(point, heightAtPosition, basin)}
+}
+
+private fun adjacentPoints(lowPoint: Pair<Int, Int>) = listOf(
+    lowPoint.copy(first = lowPoint.first + 1),
+    lowPoint.copy(first = lowPoint.first - 1),
+    lowPoint.copy(second = lowPoint.second + 1),
+    lowPoint.copy(second = lowPoint.second - 1),
+)
+
+private fun min(ints: List<Int?>): Int {
     return ints.fold(Int.MAX_VALUE) { curMin, next ->
-        val compVal = next?:Int.MAX_VALUE
+        val compVal = next ?: Int.MAX_VALUE
         if (compVal < curMin)
             compVal
         else
@@ -23,11 +49,5 @@ private fun min(vararg ints: Int?): Int {
     }
 }
 
-private fun isLowPoint(point: Pair<Int, Int>, heightAtPosition: Map<Pair<Int, Int>, Int>): Boolean {
-    return heightAtPosition[point]!! < min(
-            heightAtPosition[point.copy(first = point.first + 1)],
-            heightAtPosition[point.copy(first = point.first - 1)],
-            heightAtPosition[point.copy(second = point.second + 1)],
-            heightAtPosition[point.copy(second = point.second - 1)],
-    )
-}
+private fun isLowPoint(point: Pair<Int, Int>, heightAtPosition: Map<Pair<Int, Int>, Int>) =
+    heightAtPosition[point]!! < min(adjacentPoints(point).map { (heightAtPosition[it] ?: Int.MAX_VALUE) })
