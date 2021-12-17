a9dce8bdab0a7f3015ead361a5a10df9c23f535a
Phil Goodwin <phil.goodwin@gmail.com>
2021-12-16 17:11:52 -0800

    Day 9 -- part 1

--- src/main/kotlin/day9.kt
+++ src/main/kotlin/day9.kt
@@ -1,17 +1,33 @@
-import jdk.nashorn.internal.objects.NativeMath.min
 import java.io.File
 
+@OptIn(ExperimentalStdlibApi::class)
 fun main() {
     val rawHeights = File("lavacave_heightmap.txt").readLines()
-    val xMax = rawHeights.first().length
-    val yMax = rawHeights.size
     val heightAtPosition = mutableMapOf<Pair<Int, Int>, Int>()
     rawHeights.forEachIndexed { y, rowOfHeights ->
         rowOfHeights.forEachIndexed { x, heightChar ->
-            heightAtPosition[Pair(x,y)] = heightChar.toInt()
+            heightAtPosition[Pair(x, y)] = heightChar.digitToInt()
         }
+    }
+
+    heightAtPosition.keys.filter { isLowPoint(it, heightAtPosition) }.sumOf{ (heightAtPosition[it]!! + 1)} }
+}
+
+private fun min(vararg ints: Int?): Int {
+    return ints.fold(Int.MAX_VALUE) { curMin, next ->
+        val compVal = next?:Int.MAX_VALUE
+        if (compVal < curMin)
+            compVal
+        else
+            curMin
     }
 }
 
-privat fun isLowPoint(point: Pair<Int, Int>, heightAtPosition: Map<Pair<Int, Int>, Int>): Boolean {
+private fun isLowPoint(point: Pair<Int, Int>, heightAtPosition: Map<Pair<Int, Int>, Int>): Boolean {
+    return heightAtPosition[point]!! < min(
+            heightAtPosition[point.copy(first = point.first + 1)],
+            heightAtPosition[point.copy(first = point.first - 1)],
+            heightAtPosition[point.copy(second = point.second + 1)],
+            heightAtPosition[point.copy(second = point.second - 1)],
+    )
 }
